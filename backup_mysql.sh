4513efef7c5787cbefadc8d2e679a11ff7ff7c3b
saxonredhat <saxon.redhat@gmail.com>
2021-05-20 10:07:34 +0800

    Update backup_mysql.sh

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/mysql/bin/mysqldump -uroot -pChutong1305 --opt -R --databases lbd payment_gateway ftc >/data/backup/mysql/lbd_payment_gateway_ftc_`date +%Y%m%d_%H%M%S`.sql
+/usr/local/mysql/bin/mysqldump -uroot -pxxxxxxx --opt -R --databases lbd payment_gateway ftc >/data/backup/mysql/lbd_payment_gateway_ftc_`date +%Y%m%d_%H%M%S`.sql
